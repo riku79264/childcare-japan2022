b1c7620f7db008f3b676a4c6e5e44fdf7719358e
riku79264 <riku.tanimoto.tani@gmail.com>
2022-07-11 03:12:18 +0900

    dayカラム変更

--- db/migrate/20220702004704_create_reservations.rb
+++ db/migrate/20220702004704_create_reservations.rb
@@ -1,9 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[6.0]
   def change
     create_table :reservations do |t|
-      t.date   :day,                   null:false
-      t.string :time,                  null:false
-      t.datetime :start_time,          null:false
+      t.date   :start_date,            null: false
+      t.string :time,                  null: false
       t.string :children_number_id,    null: false
       t.string :children_name,         null: false
       t.text   :allergy
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_004704) do
+ActiveRecord::Schema.define(version: 2022_07_10_174947) do
 
   create_table "reservations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.date "day", null: false
+    t.date "start_date", null: false
     t.string "time", null: false
-    t.datetime "start_time", null: false
     t.string "children_number_id", null: false
     t.string "children_name", null: false
     t.text "allergy"
