f16b34290d5d9473a8b2dfb0caedde6330da9287
riku79264 <riku.tanimoto.tani@gmail.com>
2022-07-11 18:06:13 +0900

    validates_timeliness'アンインストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_10_174947) do
+ActiveRecord::Schema.define(version: 2022_07_02_004704) do
 
   create_table "reservations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "start_date", null: false
